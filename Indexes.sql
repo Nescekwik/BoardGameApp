cab31e5e65c4a76d4a8d246d300cbd983e214c1c
Maxime <123277467+Nescekwik@users.noreply.github.com>
2025-05-05 11:41:49 +0200

    add indexes

---
+++ Indexes.sql
@@ -0,0 +1,7 @@
+-- Indexes
+CREATE INDEX idx_users_email ON Users(email);
+CREATE INDEX idx_games_rating ON Games(rating);
+CREATE INDEX idx_reviews_user_game ON Reviews(user_id, game_id);
+CREATE INDEX idx_reviews_game_score ON Reviews(game_id, score);
+
+
